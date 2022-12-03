159d86d7978a855ae977ee801b3a953e649306ab
Bogdan <bsshul@gmail.com>
2022-12-03 10:06:50 +0200

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-import images from "./images";
+
+import { AnimatedDiv } from "./components/AnimatedDiv";
+import { AnimatedTextP } from "./components/AnimatedTextP";
+import { Caroucel } from "./components/Carousel";
+import { Label } from "./components/ui/Label";
 
 export const App = () => {
-  const [width, setWidth] = useState(0);
-  const carousel = useRef<any>();
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-  }, []);
   return (
     <div>
-      <motion.div ref={carousel} className="carousel">
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          whileTap={{ cursor: "grabbing" }}
-          className="inner-carousel"
-        >
-          {images.map((image) => (
-            <motion.div key={image} className="item">
-              <img src={image} alt="img" />
-            </motion.div>
-          ))}
-        </motion.div>
-      </motion.div>
+      <Label text="Caroucel" />
+      <Caroucel />
+
+      <div className="flex gap-10 justify-center">
+        <div>
+          <Label text="Animated Text" />
+          <AnimatedTextP />
+        </div>
+        <div>
+          <Label text="Animated Div" />
+          <AnimatedDiv />
+        </div>
+      </div>
     </div>
   );
 };
